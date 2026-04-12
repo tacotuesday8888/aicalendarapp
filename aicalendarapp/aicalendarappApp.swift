6b6a4dbc6c92750c8d4ba55472dedab434711682
langqi <langqizhao@outlook.com>
2026-04-12 15:25:17 -0400

    Initial Commit

---
+++ aicalendarapp/aicalendarappApp.swift
@@ -0,0 +1,17 @@
+//
+//  aicalendarappApp.swift
+//  aicalendarapp
+//
+//  Created by Langqi Zhao on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct aicalendarappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aicalendarappTests/aicalendarappTests.swift
@@ -0,0 +1,17 @@
+//
+//  aicalendarappTests.swift
+//  aicalendarappTests
+//
+//  Created by Langqi Zhao on 4/12/26.
+//
+
+import Testing
+@testable import aicalendarapp
+
+struct aicalendarappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
